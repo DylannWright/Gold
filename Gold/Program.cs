a3f6d9540435c979e12c1674e49ab34c798714fc
Dylan Wright <17WrighD@fulford.york.sch.uk>
2022-09-22 11:14:35 +0100

    Add project files.

---
+++ Gold/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Gold
+{
+    class Program
+    {
+        public static int ContainsGold(string p1, string p2)
+        {
+            int gold = 0;
+            p1 = p1.ToUpper();
+            p2 = p2.ToUpper();
+
+            if (p1.Contains("GOLD"))
+            {
+                gold++;
+
+                if (p2.Contains("GOLD"))
+                {
+                    gold++;
+                }
+
+            }
+            return (gold);
+        }
